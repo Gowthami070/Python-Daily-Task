a6bd5a8a1454246b48a06f8f6df44ec0088970c6
Gowthami Mareedu <gowthamimareedu18@gmail.com>
2026-07-20 23:39:34 +0530

    Day22: Basic programs on dictionary

---
+++ Day22 Task.py
@@ -0,0 +1,34 @@
+#1. Write a Python program to print all keys whose values are divisible by 5?
+'''
+Sample Input:{'A': 10, 'B': 18, 'C': 25, 'D': 31}
+Sample Output:
+A
+C
+'''
+#
+'''
+x={'A': 10, 'B': 18, 'C': 25, 'D': 31}
+for i in x:
+    if x[i]%5==0:
+        print(i)
+        '''
+#2.Write a Python program to count how many values in a dictionary are palindrome numbers?
+'''
+Sample Input:{'A': 121, 'B': 45, 'C': 131, 'D': 56}
+Sample Output:2
+'''
+#3.Write a Python program to create a new dictionary containing only the key-value pairs whose values are prime numbers?
+'''
+Sample Input:{'A': 2, 'B': 4, 'C': 5, 'D': 9, 'E': 11}
+Sample Output:{'A': 2, 'C': 5, 'E': 11}
+'''
+#4.Write a Python program using list comprehension to create a list containing only palindrome numbers from a given list.
+'''
+Sample Input:[121, 45, 131, 67, 99]
+Sample Output:[121, 131, 99]
+'''
+#5. Write a Python program using list comprehension to create a new list containing the length of each word?
+'''
+Sample Input:["apple", "cat", "elephant", "dog"]
+Sample Output:[5, 3, 8, 3]
+'''
